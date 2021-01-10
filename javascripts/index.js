8359db111ff3a1f68cacb9c051bbecaa4b760ed6
o0oke <o0oke@qq.com>
2021-01-10 10:36:37 +0800

    update js

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,29 +5,25 @@ $(document).ready(function(){
     //begins to update left list of the articles
     //can read from a single js file for article list array only.
     //ToDo: Dynamic update according to the *.md file list under post folder.
-    var table1 = document.getElementById('artlist');
-    $("#about").click( function(){
-      LoadDMFile("about");
-    });
-    $("#gallery").click( function(){
-      LoadDMFile("gallery");
-    });
-    $("#collection").click( function(){
-      LoadDMFile("collection");
-    });
-    $("#misc").click(function() {
-      LoadDMFile("misc");
-    });
-
-    //by now. all the artical and links ready, begin to enable click and show MD files.
-    var linklist = table1.getElementsByTagName('a');
-    for (i=0; i<linklist.length; i++) {
-        var sarticle = linklist[i].id;
-        $("#"+sarticle).click(function(){
-          LoadDMFile(this.id); 
-          }
-        );
-    }
+    var artList = ["about", "gallery", "collection", "misc"];
+    artList.forEach( function(mkname){
+        $("#"+mkname).click(function() {
+          LoadDMFile(mkname);
+        });
+      }
+    );
+    // $("#about").click( function(){
+    //   LoadDMFile("about");
+    // });
+    // $("#gallery").click( function(){
+    //   LoadDMFile("gallery");
+    // });
+    // $("#collection").click( function(){
+    //   LoadDMFile("collection");
+    // });
+    // $("#misc").click(function() {
+    //   LoadDMFile("misc");
+    // });
 
 });
 
