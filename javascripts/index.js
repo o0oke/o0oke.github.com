a57b2648808aadc5f1357af7b9b1903d12927764
o0oke <o0oke@qq.com>
2021-01-29 00:05:55 +0800

    list

--- javascripts/index.js
+++ javascripts/index.js
@@ -19,9 +19,13 @@ $(document).ready(function(){
 // function of loading markdown file into the showarticle section of the page.
 function LoadDMFile(sName){
     htmlobj=$.ajax({url:"post/"+sName+".md",async:false});
+    listobj=$.ajax({url:"post/"+sName+"_list.md",async:false});
     var converter = new showdown.Converter();
     var text = htmlobj.responseText; 
+    var listtext = listobj.responseText; 
     var bhtml = converter.makeHtml(text);
+    var listhtml = converter.makeHtml(listtext);
     $("#showarticle").html(bhtml);
+    $("#artlist").html(listhtml);
     $("html, body").animate({ scrollTop: $("#showarticle").offset().top }, "slow"); 
 }
