20da1cee93b3dbd45dd10fe179398848b1c20c98
o0oke <o0oke@qq.com>
2021-10-11 07:37:42 +0800

    add chatra.js

---
+++ javascripts/chatra.js
@@ -0,0 +1,16 @@
+
+      window.ChatraSetup = {
+          chatWidth: 360,
+          chatHeight: 520
+      };
+
+      (function(d, w, c) {
+        w.ChatraID = 'MmZEQ4247L2FRgB2P';
+        var s = d.createElement('script');
+        w[c] = w[c] || function() {
+          (w[c].q = w[c].q || []).push(arguments);
+        };
+        s.async = true;
+        s.src = 'https://call.chatra.io/chatra.js';
+        if (d.head) d.head.appendChild(s);
+      })(document, window, 'Chatra');
